e60b3fa09e5011da3ac8e45a05c9f1f34c9fee81
胡小忠 <1466947023@qq.com>
2018-02-02 22:32:45 +0800

    同步本地

---
+++ test/com/mrhu/amazed/hibernatedata/TranscriptFactoryTest.java
@@ -0,0 +1,14 @@
+package com.mrhu.amazed.hibernatedata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TranscriptFactoryTest {
+
+	@Test
+	public void test() {
+		System.out.println(TranscriptFactory.getTranscript("678张"));
+	}
+
+}
